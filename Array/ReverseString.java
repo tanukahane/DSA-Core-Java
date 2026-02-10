b9bd94a601a978752e0b6ee70eb855d63d4b7daa
tanukahane <158300165+tanukahane@users.noreply.github.com>
2026-02-10 18:09:09 +0530

    A program receives a string as input. You are required to reverse the given string and display the result. Do not use any built-in reverse functions.

---
+++ Array/ReverseString.java
@@ -0,0 +1,14 @@
+public class ReverseString {
+    public static void main(String[] args) {
+        String s = "hello";
+        String r = "";
+
+        for(int i= s.length()-1 ; i >= 0 ; i-- ){
+            r = r + s.charAt(i);
+
+        }
+        System.out.println("original word : " + s );
+        System.out.println("reversed word : " + r );
+    }
+    
+}
